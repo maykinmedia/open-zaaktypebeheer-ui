7ebc27036b97a2d37aa202a1c4a177f01945318b
stefrado <109965488+stefrado@users.noreply.github.com>
2023-08-15 13:08:29 +0200

    :construction_worker: #6 Create ci.yml

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider, RequireAuth } from './components/Auth/Auth';
+import { AuthProvider, RequireAuth, RequireNoAuth } from './components/Auth/Auth';
 import BaseView from './views/BaseView';
 import LoginView from './views/LoginView';
 
@@ -10,13 +10,11 @@ function App() {
         <Route element={<RequireAuth />}>
           <Route path="/" element={<BaseView />}>
             <Route path="/" element={<BaseView />} />
-            <Route path="/zaaktypen" element={<BaseView />} />
-            <Route path="/zaaktypen/:zaaktype" element={<BaseView />} />
-            <Route path="/attributen" element={<BaseView />} />
-            <Route path="/attributen/:attribuut" element={<BaseView />} />
           </Route>
         </Route>
-        <Route path="/login" element={<LoginView />}></Route>
+        <Route element={<RequireNoAuth />}>
+          <Route path="/login" element={<LoginView />} />
+        </Route>
       </Routes>
     </AuthProvider>
   );
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,19 +1,16 @@
-/**
- * Based on primeblocks example login view
- * @link https://blocks.primereact.org/free#:~:text=85%20responded-,Sign%2DIn,-Free
- */
-
-import { Checkbox, CheckboxChangeEvent } from 'primereact/checkbox';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import LoginIcon from '@mui/icons-material/Login';
+import { Box, Button, Paper, TextField, Typography } from '@mui/material';
+// Same as below, will return in next PR
+// import PasswordField from '../components/Fields/Password';
 import { useAuth } from '../components/Auth/Auth';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { Password } from 'primereact/password';
+import { useNavigate } from 'react-router-dom';
 import logo from '/logo.svg';
 
 export default function LoginView() {
-  const [checked, setChecked] = useState(false);
+  const [user, setUser] = useState('');
+  // Same as below and above, will return in next PR
+  // const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const auth = useAuth();
   const navigate = useNavigate();
@@ -22,69 +19,70 @@ export default function LoginView() {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     try {
-      auth.signIn(data, () => {
+      await auth.signIn(data, () => {
         navigate('/', { replace: true });
       });
-    } catch (error: any) {
-      setError(error?.message as string);
+    } catch (err) {
+      setError(err as string);
     }
   }
 
   return (
-    <main className="surface-ground h-screen">
-      <section className="flex align-items-center h-full">
-        <section className="flex flex-column align-items-center m-auto justify-content-center surface-card p-4 shadow-1 border-round w-11 max-w-30rem lg:w-6">
-          <section className="text-center mb-3">
-            <img src={logo} alt="logo" height={40} className="mb-3" />
-            <h1 className="text-800 text-4xl font-bold mb-3">OpenZaak Beheer</h1>
-          </section>
-          <form className="w-full" onSubmit={handleLogin}>
-            <label htmlFor="email" className="block text-900 font-medium mb-2">
-              Email
-            </label>
-            <InputText
-              name="username"
-              id="username"
-              type="text"
-              placeholder="Gebruikersnaam"
-              className="w-full mb-3"
-            />
+    <Box
+      component={'main'}
+      sx={{
+        minHeight: '100dvh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Paper
+        elevation={3}
+        component={'form'}
+        onSubmit={handleLogin}
+        sx={{
+          margin: 'auto',
+          padding: '2rem',
+          gap: '1rem',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          width: '90%',
+          maxWidth: '30rem',
+        }}
+      >
+        <img src={logo} alt="logo" height={50} />
+        <Typography variant="h4" component={'h1'}>
+          Login
+        </Typography>
+        {error && <p>{error}</p>}
 
-            <label htmlFor="password" className="block text-900 font-medium mb-2">
-              Password
-            </label>
-            <Password
-              name="password"
-              autoComplete="password"
-              id="password"
-              type="password"
-              placeholder="Wachtwoord"
-              className="w-full mb-3"
-              feedback={false}
-              inputStyle={{ width: '100%' }}
-              toggleMask
-            />
-            {error && <p className="text-500 mb-4">{error}</p>}
-
-            <div className="flex align-items-center justify-content-between mb-5">
-              <div className="flex align-items-center gap-2">
-                <Checkbox
-                  onChange={(e: CheckboxChangeEvent) => setChecked(e.checked!)}
-                  checked={checked}
-                />
-                <label className="text-600" htmlFor="rememberme">
-                  Onthoud mij
-                </label>
-              </div>
-              <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">
-                Wachtwoord vergeten?
-              </a>
-            </div>
-
-            <Button label="Log in" icon="pi pi-user" className="w-full" />
-          </form>
-        </section>
-      </section>
-    </main>
+        <TextField
+          variant="filled"
+          label="Gebruikersnaam"
+          name="username"
+          id="username"
+          value={user}
+          type={'text'}
+          autoComplete="username"
+          onChange={(e) => setUser(e.target.value)}
+        />
+        {/* 
+        I commented these out for now, due to the discussed error
+        Next PR i will add this field again
+        <PasswordField password={password} setPassword={setPassword} /> 
+         */}
+        <Button
+          color={'primary'}
+          startIcon={<LoginIcon />}
+          variant="contained"
+          type="submit"
+          aria-label="Login"
+        >
+          Login
+        </Button>
+      </Paper>
+    </Box>
   );
 }
